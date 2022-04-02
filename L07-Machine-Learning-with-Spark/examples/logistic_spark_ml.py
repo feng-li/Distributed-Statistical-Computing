1b97771b8a152ad990c3c3dc062ec4f20bba11ff
Feng Li <m@feng.li>
2022-04-02 16:33:39 +0800

    Update examples

--- L07-Machine-Learning-with-Spark/examples/logistic_spark_ml.py
+++ L07-Machine-Learning-with-Spark/examples/logistic_spark_ml.py
@@ -16,12 +16,50 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
 
-from dlsa.models import simulate_logistic
+# from dlsa.models import simulate_logistic
 
 import numpy as np
 import pandas as pd
 import time
 import sys
+
+
+def simulate_logistic(sample_size, p, partition_method, partition_num):
+    '''Simulate data based on logistic model
+
+    '''
+    ## Simulate Data
+    n = sample_size
+    p1 = int(p * 0.4)
+
+    # partition_method = "systematic"
+    # partition_num = 200
+
+    ## TRUE beta
+    beta = np.zeros(p).reshape(p, 1)
+    beta[:p1] = 1
+
+    ## Simulate features
+    features = np.random.rand(n, p) - 0.5
+    prob = 1 / (1 + np.exp(-features.dot(beta)))
+
+    ## Simulate label
+    label = np.zeros(n).reshape(n, 1)
+    partition_id = np.zeros(n).reshape(n, 1)
+    for i in range(n):
+        # TODO: REMOVE loop
+        label[i] = np.random.binomial(n=1,p=prob[i], size=1)
+
+        if partition_method == "systematic":
+            partition_id[i] = i % partition_num
+        else:
+            raise Exception("No such partition method implemented!")
+
+        data_np = np.concatenate((partition_id, label, features), 1)
+        data_pdf = pd.DataFrame(data_np, columns=["partition_id"] + ["label"] + ["x" + str(x) for x in range(p)])
+
+    return data_pdf
+
 
 tic0 = time.perf_counter()
 ##----------------------------------------------------------------------------------------
