aa65b8a060625a2d75a30006d6dfa21e8a376893
Feng Li <m@feng.li>
2023-06-08 17:49:36 +0800

    Add local Spark config

--- setup_spark.sh
+++ setup_spark.sh
@@ -3,7 +3,7 @@
 #       source setup_spark.sh
 
 export JAVA_HOME=/usr/lib/jvm/default-java
-export SPARK_HOME=${HOME}/.APP/spark/
+export SPARK_HOME=${HOME}/.APP/spark
 export PATH=${SPARK_HOME}/bin:$PATH
 
 export PYARROW_IGNORE_TIMEZONE=1
