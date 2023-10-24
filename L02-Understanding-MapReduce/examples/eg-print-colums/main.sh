8c44100668f68ebe6f5b7d7a5584b2f125bc1b2e
Feng Li <m@feng.li>
2023-10-24 21:38:09 +0800

    Update SSH forwarding

--- L02-Understanding-MapReduce/examples/eg-print-colums/main.sh
+++ L02-Understanding-MapReduce/examples/eg-print-colums/main.sh
@@ -2,7 +2,6 @@
 
 # Current code directory
 PWD=$(cd $(dirname $0); pwd)
-cd $PWD 1> /dev/null 2>&1
 
 # Asign a task name 
 TASKNAME=task1_lifeng
@@ -17,9 +16,9 @@ HADOOP_OUTPUT_DIR=/user/lifeng/results/task1
 echo $HADOOP_INPUT_DIR
 echo $HADOOP_OUTPUT_DIR
 
-$HADOOP_HOME fs -rmr $HADOOP_OUTPUT_DIR
+hadoop fs -rmr $HADOOP_OUTPUT_DIR
 
-hadoop -jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
     -jobconf mapred.job.name=$TASKNAME \
     -jobconf mapred.job.priority=NORMAL \
     -jobconf mapred.map.tasks=500 \
@@ -29,21 +28,14 @@ hadoop -jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
     -jobconf stream.num.map.output.key.fields=2 \
     -jobconf mapred.text.key.partitioner.options=-k1,1 \
     -jobconf stream.memory.limit=1000 \
-    -file $PWD/mapper.sh \
     -output ${HADOOP_OUTPUT_DIR} \
     -input ${HADOOP_INPUT_DIR} \
     -mapper "sh mapper.sh" \
-    -reducer "cat" \
+    -reducer "/usr/bin/cat" \
+    -file $PWD/mapper.sh \
     -cacheArchive ${PY27}#py27 \
     -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner
 
 
-if [ $? -ne 0 ]; then
-    echo 'error'
-    exit 1
-fi
-$HADOOP_HOME fs -touchz ${HADOOP_OUTPUT_DIR}/done
-
-exit 0
-
+hadoop fs -ls ${HADOOP_OUTPUT_DIR}/_SUCCESS
 
