bdcca8a102c058ac7c0aa629287002b9757226e9
Feng Li <m@feng.li>
2023-10-27 10:18:04 +0800

    Update bash file

--- L02-Understanding-MapReduce/examples/eg-bash+python/main.sh
+++ L02-Understanding-MapReduce/examples/eg-bash+python/main.sh
@@ -6,9 +6,8 @@ cd $PWD 1> /dev/null 2>&1
 TASKNAME=hadoop_task_fli
 
 # hadoop client
-HADOOP_INPUT_DIR=/fli/data/part-*
-HADOOP_OUTPUT_DIR=/fli/results/task
-HADOOP_VERSION=3.1.3
+HADOOP_INPUT_DIR=/data/used_car_data* # HDFS path
+HADOOP_OUTPUT_DIR=/fli/results/task  # HDFS path
 
 echo $HADOOP_HOME
 echo $HADOOP_INPUT_DIR
@@ -16,21 +15,20 @@ echo $HADOOP_OUTPUT_DIR
 
 hadoop fs -rm -r $HADOOP_OUTPUT_DIR
 
-hadoop jar  ${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-streaming-${HADOOP_VERSION}.jar \
+hadoop jar  ${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-streaming-*.jar \
        -D mapred.job.name=TASKNAME \
        -D mapred.job.priority=NORMAL \
        -D stream.memory.limit=1000 \
        -D mapred.map.tasks=10 \
-       -D mapred.reduce.tasks=7 \
+       -D mapred.reduce.tasks=7 \ 
        -D mapred.job.map.capacity=100 \
        -D mapred.job.map.capacity=100 \
        -input ${HADOOP_INPUT_DIR} \
        -output ${HADOOP_OUTPUT_DIR} \
-       -mapper "$PWD/mapper.sh" \
-       -file "$PWD/mapper.sh"
-# -cacheArchive ${PY27}#py27 \
-# -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner
+       -mapper "sh mapper.sh" \
+       -file "/home/teacher01/code/mapper.sh"
 
+# -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner
 # following are others generic command, if use ,please put them above streaming Options
 # -D mapred.compress.map.output=True # if output of map is compressed
 # -D mapred.map.output.comression.codec=org.apache.hadoop.io.compress.GzipCodec # compress mode
